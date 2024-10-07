b45f32d0fd80e4b80356b192eb54f73362f2670f
ppompon <matty.pulacatula@gmail.com>
2024-10-07 11:05:15 -0300

    calculadora suprema

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // Dados
-    // Botões de múltiplas classes;
-    // Botões de classes únicas;
-    // Valor do display;
+    // Botões de múltiplas classes; <{+}>
+    // Botões de classes únicas; <{+}>
+    // Valor do display; <{+}>
 
 // Display
-    // Captura do valor atual;
-    // Verificação de ponto;
+    // Captura do valor atual; <{+}>
+    // Verificação de ponto; <{+}>
 
 // Operação
-    // Dados do último número digitado salvo;
-    // Reinício do display após a operação selecionada;
-    // Conversão do número em string para number;
-    // Operação do número atual com o número anterior;
+    // Dados do último número digitado salvo; <{+}>
+    // Reinício do display após a operação selecionada; <{+}>
+    // Conversão do número em string para number; <{+}>
+    // Operação do número atual com o número anterior; <{+}>
 
 // Eventos
-    // Atribuição dos eventos para botões múltiplos ou únicos;
+    // Atribuição dos eventos para botões múltiplos ou únicos; <{+}>
 
 // REALIZAÇÃO DO PROJETO //
 
@@ -23,18 +23,73 @@ let operators = document.querySelectorAll(".operator");
 let point = document.querySelector(".point");
 let equal = document.querySelector(".equal");
 let display = document.querySelector("input");
+let cbtn = document.querySelector("#C");
 
+var lastNumber = "";
 var actualNumber = "";
+var operator = "";
 
-function showDisplay() {
-    display.value += actualNumber;
+// Functions
+function c() {
+    display.value = "";
+    lastNumber = "";
+    actualNumber = "";
 }
 
+function lastNumberfx(event) {
+    var convert = parseFloat(event);
+    return convert
+}
+function reset(event) {
+    operator = event.target.textContent;
+    console.log(operator);
+    var test = lastNumberfx(display.value);
+    lastNumber = test;
+    display.value = "";
+}
+
+function calc() {
+    var convert = display.value;
+    convert = parseFloat(convert);
+    switch (operator) {
+        case "+":
+            var e = lastNumber + convert;
+            display.value = e;
+            break;
+        case "-":
+            var e = lastNumber - convert;
+            display.value = e;
+            break;
+        case "*":
+            var e = lastNumber * convert;
+            display.value = e;
+            break;
+        case "/":
+            var e = lastNumber / convert;
+            display.value = e;
+            break; 
+    }
+}
+function pointDisplay() {
+    if (display.value.indexOf(".") === -1) {
+        console.log("Verificação");
+        actualNumber = ".";
+        console.log("Ponto Inserido");
+        showDisplay();
+    }
+}
 function inputCapture(event){
     actualNumber = event.target.textContent;
-    console.log(actualNumber);
-
     showDisplay();
 }
+function showDisplay() {
+    display.value += actualNumber;
+    console.log("Display Atualizado");
+}
 
+// Events
+cbtn.addEventListener("click", c);
+equal.addEventListener("click", calc);
+point.addEventListener("click", pointDisplay);
+operators.forEach((button) => button.addEventListener("click", reset));
 numbers.forEach((button) => button.addEventListener("click", inputCapture));
